ed75343d995fb2242eef52114c99e7cdd1b89047
bramba2000 <54905332+bramba2000@users.noreply.github.com>
2022-04-04 11:13:48 +0200

    Add skeleton of TurnManager and GameManager class - code not working and tests needed small fix after refactoring the StudentSpaceException in the new package

--- src/main/java/it/polimi/ingsw/am37/model/Bag.java
+++ src/main/java/it/polimi/ingsw/am37/model/Bag.java
@@ -1,7 +1,9 @@
 package it.polimi.ingsw.am37.model;
 
 import it.polimi.ingsw.am37.model.student_container.LimitedStudentsContainer;
+import it.polimi.ingsw.am37.model.student_container.StudentsContainer;
 import it.polimi.ingsw.am37.model.student_container.UnlimitedStudentsContainer;
+
 import java.util.Random;
 
 /**
@@ -9,56 +11,65 @@ import java.util.Random;
  */
 public class Bag {
 
-	/**
-	 * It represents the number of students in the bag when the game starts.
-	 */
-	private final static int startingStudentsPerColor = 24;
+    /**
+     * It represents the number of students in the bag when the game starts.
+     */
+    private final static int startingStudentsPerColor = 24;
+    /**
+     * It represents the students in the Bag.
+     */
+    private final UnlimitedStudentsContainer studentsAvailable;
 
-	/**
-	 * Default constructor, it fills the Bag with 2 students from each Faction.
-	 */
-	public Bag() {
-		studentsAvailable = new UnlimitedStudentsContainer();
-		for(FactionColor color : FactionColor.values())
-			studentsAvailable.addStudents(startingStudentsPerColor, color);
-	}
+    /**
+     * Default constructor, it fills the Bag with 2 students from each Faction.
+     */
+    public Bag() {
+        studentsAvailable = new UnlimitedStudentsContainer();
+        for (FactionColor color : FactionColor.values())
+            studentsAvailable.addStudents(startingStudentsPerColor, color);
+    }
 
-	/**
-	 * It represents the students in the Bag.
-	 */
-	private final UnlimitedStudentsContainer studentsAvailable;
+    /**
+     * It extracts students from the Bag.
+     *
+     * @param num The number of students to extracts.
+     * @return A LimitedStudentContainer that contains the students extracted by FactionColor.
+     * @throws IllegalArgumentException If there are not enough students to extract.
+     */
+    public LimitedStudentsContainer extractStudents(int num) {
+        if (studentsAvailable.size() < num)
+            throw new IllegalArgumentException("there are not enough students to extract");
+        LimitedStudentsContainer studentsExtracted;
+        int[] colorBound = new int[]{0, 0, 0, 0, 0};
+        Random colorGenerator = new Random();
+        FactionColor colorExtracted;
+        for (int i = 0; i < num; i++) {
+            colorExtracted = FactionColor.values()[colorGenerator.nextInt(FactionColor.values().length)];
+            if (studentsAvailable.getByColor(colorExtracted) > colorBound[colorExtracted.getIndex()])
+                colorBound[colorExtracted.getIndex()]++;
+            else i--;
+        }
+        studentsExtracted = new LimitedStudentsContainer(colorBound);
+        for (FactionColor color : FactionColor.values()) {
+            studentsExtracted.addStudents(colorBound[color.getIndex()], color);
+            studentsAvailable.removeStudents(colorBound[color.getIndex()], color);
+        }
+        return studentsExtracted;
+    }
 
-	/**
-	 * It extracts students from the Bag.
-	 * @param num The number of students to extracts.
-	 * @return A LimitedStudentContainer that contains the students extracted by FactionColor.
-	 * @throws IllegalArgumentException If there are not enough students to extract.
-	 */
-	public LimitedStudentsContainer extractStudents(int num) {
-		if(studentsAvailable.size() < num)
-			throw new IllegalArgumentException("there are not enough students to extract");
-		LimitedStudentsContainer studentsExtracted;
-		int[] colorBound = new int[]{0,0,0,0,0};
-		Random colorGenerator = new Random();
-		FactionColor colorExtracted;
-		for(int i = 0; i < num; i++) {
-			colorExtracted = FactionColor.values()[colorGenerator.nextInt(FactionColor.values().length)];
-			if(studentsAvailable.getByColor(colorExtracted) > colorBound[colorExtracted.getIndex()])
-				colorBound[colorExtracted.getIndex()]++;
-			else i--;
-		}
-		studentsExtracted = new LimitedStudentsContainer(colorBound);
-		for(FactionColor color : FactionColor.values()) {
-			studentsExtracted.addStudents(colorBound[color.getIndex()], color);
-			studentsAvailable.removeStudents(colorBound[color.getIndex()], color);
-		}
-		return studentsExtracted;
-	}
+    /**
+     * @return the number of the students in the Bag.
+     */
+    public int size() {
+        return studentsAvailable.size();
+    }
 
-	/**
-	 * @return the number of the students in the Bag.
-	 */
-	public int size(){
-		return studentsAvailable.size();
-	}
+    /**
+     * Add students to the bag
+     *
+     * @param container the student to add to bag
+     */
+    public void addStudents(StudentsContainer container) {
+        studentsAvailable.uniteContainers(container);
+    }
 }
---
+++ src/main/java/it/polimi/ingsw/am37/model/TurnManager.java
@@ -0,0 +1,39 @@
+package it.polimi.ingsw.am37.model;
+
+import it.polimi.ingsw.am37.model.student_container.LimitedStudentsContainer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TurnManager {
+    private ArrayList<Player> players;
+    private HashMap<Player, Assistant> assistantPlayed;
+
+    public void addStudentsToDining(LimitedStudentsContainer container) {
+
+    }
+
+    public void removeStudentsFromDining(LimitedStudentsContainer container) {
+
+    }
+
+    public void useAssistant(Player player, Assistant assistant) {
+
+    }
+
+    public void setUp(Bag bag, int numOfPlayers) {
+
+    }
+
+    public void studentCloudToEntrance(Cloud cloud) {
+
+    }
+
+    public void studentsEntranceToIsland(Island island, int num, FactionColor color) {
+
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+}
