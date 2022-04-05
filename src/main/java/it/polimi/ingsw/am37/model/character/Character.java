d568de1e42d04bbf8cdca46c0403bf1ab41f41d5
Paolo <paolo2.cerutti@mail.polimi.it>
2022-04-05 13:26:34 +0200

    added Character & CharacterEffect

--- src/main/java/it/polimi/ingsw/am37/model/character/Character.java
+++ src/main/java/it/polimi/ingsw/am37/model/character/Character.java
@@ -1,53 +1,57 @@
 package it.polimi.ingsw.am37.model.character;
 
 /**
- *
+ * This class represents the Character in the game.
  */
-public class Character implements CharacterEffect {
+public class Character {
 
     /**
-     *
+     * The effect that distinguishes the character.
      */
-    private int startPrice;
+    private final CharacterEffect effect;
     /**
-     *
+     * The id of the Effect.
+     */
+    private final Effect effectId;
+    /**
+     * The starting price of the Character, it can change during the game.
+     */
+    private final int startPrice;
+    /**
+     * The price of the character in the current game state.
      */
     private int currentPrice;
-    /**
-     *
-     */
-    private CharacterEffect effect;
-
-    /**
-     *
-     */
-    private Effect effectId;
 
     /**
      * Default constructor
      */
-    public Character() {
+    public Character(int startPrice, Effect character, CharacterEffect effect) {
+        this.startPrice = startPrice;
+        this.currentPrice = startPrice;
+        this.effectId = character;
+        this.effect = effect;
     }
 
     /**
-     *
+     * it updates the cost of the Character after being used.
      */
     private void increasePrice() {
-        // TODO implement here
+        currentPrice++;
     }
 
     /**
-     * @return current price of the character
+     * @return current price of the character.
      */
     public int getCurrentPrice() {
         return currentPrice;
     }
 
     /**
-     * @param option the option parameters
+     * @param option The option parameters used to use the Effect.
      */
     public void useEffect(Option option) {
-        // TODO implement here
+        effect.useEffect(option);
+        increasePrice();
     }
 
 }
