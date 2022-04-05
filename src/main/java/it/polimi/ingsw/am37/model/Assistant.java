296850aef3f4c8819744e04dfd00d7daa906b9fe
Paolo <paolo2.cerutti@mail.polimi.it>
2022-04-05 17:24:24 +0200

    Revert "General improvements"
    This reverts commit 12f48e0db2d7010458ae6a2bb479c7acb6fbc9a7.

--- src/main/java/it/polimi/ingsw/am37/model/Assistant.java
+++ src/main/java/it/polimi/ingsw/am37/model/Assistant.java
@@ -3,32 +3,28 @@ package it.polimi.ingsw.am37.model;
 /**
  * There are ten assistant cards for each wizard, if possible equal cards can't be used in the same turn
  */
-public class Assistant {
+public class Assistant extends Wizard {
 
 	/**
-	 * It represents the team to which the assistant belongs.
+	 * Default constructor
 	 */
-	private final WizardTeam team;
+	public Assistant(WizardTeam team, int cardValue, int moveMNMovement) {
+		super(team);
+		this.cardValue = cardValue;
+		this.moveMNMovement = moveMNMovement;
+	}
 
 	/**
 	 * Each assistant has a cardValue which is useful to establish the order of players'
 	 * turns, a lower value gives more priority when deciding the order
 	 */
 	private final int cardValue;
+
 	/**
 	 * Each assistant has a moveMNMovement which specific the maximum number of islands on which you
 	 * can move MotherNature in clockwise
 	 */
 	private int moveMNMovement;
-
-	/**
-	 * Default constructor
-	 */
-	public Assistant(WizardTeam team, int cardValue, int moveMNMovement) {
-		this.team = team;
-		this.cardValue = cardValue;
-		this.moveMNMovement = moveMNMovement;
-	}
 
 
 	/**
