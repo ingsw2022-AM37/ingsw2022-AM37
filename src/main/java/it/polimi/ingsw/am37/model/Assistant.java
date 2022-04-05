4bb2cf589b9c4d4347f13325f36d3526bd563fe0
bramba2000 <54905332+bramba2000@users.noreply.github.com>
2022-04-05 23:34:59 +0200

    Small fix after 5cbc677f9a2d6a3df02d931654115286cc3232b1

--- src/main/java/it/polimi/ingsw/am37/model/Assistant.java
+++ src/main/java/it/polimi/ingsw/am37/model/Assistant.java
@@ -3,45 +3,49 @@ package it.polimi.ingsw.am37.model;
 /**
  * There are ten assistant cards for each wizard, if possible equal cards can't be used in the same turn
  */
-public class Assistant extends Wizard {
+public class Assistant {
 
-	/**
-	 * Default constructor
-	 */
-	public Assistant(WizardTeam team, int cardValue, int moveMNMovement) {
-		super(team);
-		this.cardValue = cardValue;
-		this.moveMNMovement = moveMNMovement;
-	}
+    /**
+     * Represents the team to which the assistant belongs.
+     */
+    private final WizardTeam team;
 
-	/**
-	 * Each assistant has a cardValue which is useful to establish the order of players'
-	 * turns, a lower value gives more priority when deciding the order
-	 */
-	private final int cardValue;
+    /**
+     * Each assistant has a cardValue which is useful to establish the order of players'
+     * turns, a lower value gives more priority when deciding the order
+     */
+    private final int cardValue;
 
-	/**
-	 * Each assistant has a moveMNMovement which specific the maximum number of islands on which you
-	 * can move MotherNature in clockwise
-	 */
-	private int moveMNMovement;
+    /**
+     * Each assistant has a moveMNMovement which specific the maximum number of islands on which you
+     * can move MotherNature in clockwise
+     */
+    private int moveMNMovement;
 
+    /**
+     * Default constructor
+     */
+    public Assistant(WizardTeam team, int cardValue, int moveMNMovement) {
+        this.team = team;
+        this.cardValue = cardValue;
+        this.moveMNMovement = moveMNMovement;
+    }
 
-	/**
-	 * @return the value of the card
-	 */
-	public int getCardValue() {
-		return this.cardValue;
-	}
+    /**
+     * @return the value of the card
+     */
+    public int getCardValue() {
+        return this.cardValue;
+    }
 
-	/**
-	 * @return the possible movement for MotherNature
-	 */
-	public int getMNMovement() {
-		return this.moveMNMovement;
-	}
+    /**
+     * @return the possible movement for MotherNature
+     */
+    public int getMNMovement() {
+        return this.moveMNMovement;
+    }
 
-	public void increaseMNMovement(int i) {
-		this.moveMNMovement += i;
-	}
+    public void increaseMNMovement(int i) {
+        this.moveMNMovement += i;
+    }
 }
