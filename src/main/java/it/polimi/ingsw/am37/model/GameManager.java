69f3b5dcc91426cf08c6aa0f7e8680ea08220fcd
bramba2000 <54905332+bramba2000@users.noreply.github.com>
2022-04-11 10:59:35 +0200

    Game manager first working release and tests

--- src/main/java/it/polimi/ingsw/am37/model/GameManager.java
+++ src/main/java/it/polimi/ingsw/am37/model/GameManager.java
@@ -1,87 +1,95 @@
 package it.polimi.ingsw.am37.model;
 
-import it.polimi.ingsw.am37.model.exceptions.WinningException;
-import it.polimi.ingsw.am37.model.student_container.UnlimitedStudentsContainer;
+import it.polimi.ingsw.am37.model.character.Character;
+import it.polimi.ingsw.am37.model.character.CharacterEffect;
+import it.polimi.ingsw.am37.model.character.Effect;
+import it.polimi.ingsw.am37.model.character.Option;
+import it.polimi.ingsw.am37.model.exceptions.AssistantImpossibleToPlay;
+import it.polimi.ingsw.am37.model.student_container.StudentsContainer;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class GameManager {
     /**
-     * Represent the number of the players of the game this manage
+     * Number of characters available
      */
-    private final int numOfPlayer;
-
+    private static final int NUMBEROFCHARACTERS = 3;
     /**
-     * Flag for enabling expert mode rules
+     * Number of player in the game handled by this manager
      */
-    private final boolean expertMode;
-
+    private final int playersNumber;
     /**
-     * A simple array of not used professors
+     * Flag for game mode used: FALSE represent basic rules, TRUE represent advanced rules
      */
-    private boolean[] notUsedTeachers = new boolean[FactionColor.values().length];
-
+    private final boolean advancedMode;
     /**
-     * The bag from where students tiles are extracted
-     */
-    private Bag bag;
-
-    /**
-     * A list of the clouds active in the game
-     */
-    private ArrayList<Cloud> clouds;
-
-    /**
-     * A list of active characters
-     */
-    private Character[] characters = new Character[3];
-
-    /**
-     * The island manager to handle islands and mother nature action
-     */
-    private IslandsManager islandsManager;
-
-    /**
-     * The turn manager to handle players, professors and turn handling
-     */
-    private TurnManager turnManager;
-
-    /**
-     * Construct a game manager
+     * Array of professors (represented as boolean if they are present or not) not assigned in any boards
      *
-     * @param numOfPlayer the number of players in the match
-     * @param expertMode  if the game is in expert mode
+     * @see FactionColor for color definition of each prof
      */
-    public GameManager(int numOfPlayer, boolean expertMode) {
-        this.numOfPlayer = numOfPlayer;
-        this.expertMode = expertMode;
+    private final boolean[] notUsedTeachers;
+
+    /**
+     * Model of the game bag from where students are extracted by players
+     */
+    private final Bag bag;
+
+    /**
+     * List of all cloud available in a game.
+     */
+    private final ArrayList<Cloud> clouds;
+
+    /**
+     * Array of characters available in this game
+     */
+    final private Character[] characters;
+
+    /**
+     * Reference of the manager of islands logic. Used for accessing islands and their functionality
+     */
+    private final IslandsManager islandsManager;
+
+    /**
+     * Reference of the manager of turns logic and player interconnections
+     */
+    private final TurnManager turnManager;
+
+    /**
+     * Default constructor of game manager class. It's the main access point of the game model
+     *
+     * @param playersNumber Number of player of this instance of game
+     * @param advancedMode  Enable advanced mode or disable it
+     */
+    public GameManager(int playersNumber, boolean advancedMode) {
+        this.playersNumber = playersNumber;
+        this.advancedMode = advancedMode;
+        this.clouds = new ArrayList<>();
+        this.turnManager = new TurnManager(advancedMode, playersNumber);
+        this.islandsManager = new IslandsManager();
+        this.characters = new Character[NUMBEROFCHARACTERS];
+        this.notUsedTeachers = new boolean[FactionColor.values().length];
+        this.bag = new Bag();
     }
 
     /**
-     * Prepare a model for hosting a game: set up the managers and call their sets up methods, reset to default variables state.
-     * Should be used after the constructor before starting the game
-     *
-     * @see IslandsManager
-     * @see TurnManager
+     * @return Array of unused teachers
      */
-    public void prepareGame(Character[] characters) {
-        islandsManager = new IslandsManager();
-        islandsManager.setUp();
-        turnManager = new TurnManager();
-        turnManager.setUp();
-        fillBag();
-        //num of players is the same as number of clouds
-        clouds = new ArrayList<>();
-        for (int i = 0; i < numOfPlayer; i++) {
-            clouds.add(new Cloud(numOfPlayer != 3));
-        }
-        if (expertMode)
-            this.characters = characters;
-        //TODO add logic for characters setup
-        for (FactionColor color :
-                FactionColor.values()) {
-            notUsedTeachers[color.getIndex()] = true;
-        }
+    public boolean[] getNotUsedTeachers() {
+        return notUsedTeachers;
+    }
+
+    /**
+     * @return The bag
+     */
+    public Bag getBag() {
+        return bag;
+    }
+
+    /**
+     * @return List of clouds of this game
+     */
+    public ArrayList<Cloud> getClouds() {
+        return clouds;
     }
 
     /**
@@ -99,65 +107,106 @@ public class GameManager {
     }
 
     /**
-     * Fill the bag with all the students lefts
+     * Set up the game following rules settled in the creation of the game manager: create, organize and fill the
+     * islands, the bag and the clouds list; after initialize turn manager and if rules are enabled create the
+     * characters.
      */
-    private void fillBag() {
-        final int initialStudentsForEachColor = 24;
-        bag = new Bag();
-        UnlimitedStudentsContainer container = new UnlimitedStudentsContainer();
-        for (FactionColor color :
-                FactionColor.values()) {
-            container.addStudents(initialStudentsForEachColor, color);
+    public void prepareGame() {
+        //constants for set up of the game
+        final Map<Integer, Integer> numberCloudsForPlayers = Map.of(2, 2, 3, 3, 4, 4);
+        //follow the order of the manual of the game
+        islandsManager.setUp();
+        for (int i = 0; i < numberCloudsForPlayers.get(playersNumber); i++) {
+            clouds.add(new Cloud(playersNumber == 2));
         }
-        bag.addStudents(container);
+        Arrays.fill(notUsedTeachers, true);
+        turnManager.setUp(bag);
+        //TODO handle assistants logic
+
+        // advanced logic only
+        if (this.advancedMode) {
+            List<Effect> temp = new ArrayList<>(Arrays.stream(Effect.values()).toList());
+            Collections.shuffle(temp);
+            for (int i = 0; i < NUMBEROFCHARACTERS; i++) {
+                Effect effect = temp.get(i);
+                characters[i] = new Character(effect.getInitialPrice(), effect, new CharacterEffect(effect, bag));
+            }
+        }
     }
 
     /**
-     * Reset the turn for the next player
-     */
-    public void resetTurn() {
-        islandsManager.resetFlag();
-        turnManager.resetTurn();
-    }
-
-    /**
-     * Check the end of the game.
+     * Move the students inside the container into an island provided
      *
-     * @throws WinningException thrown one of the winning conditions is matched
+     * @param container The students to move
+     * @param islandId  The id of destination island where students are moved
+     * @throws IllegalArgumentException When the container is null
      */
-    public void checkWinCondition(boolean endTurn) throws WinningException {
-        boolean victory = false;
-        for (Player player :
-                turnManager.getPlayers()) {
-            if (player.getBoard().getTowers().getCurrentSize() == 0 && numOfPlayer != 4)
-                throw new WinningException(player);
-            if (islandsManager.getIslands().size() <= 3 || (endTurn && (player.getAssistantsDeck().size() == 0 || bag.size() == 0)))
-                victory = true;
+    public void moveStudentsToIsland(StudentsContainer container, int islandId) throws IllegalArgumentException {
+        if (container == null) {
+            throw new IllegalArgumentException("container of moveStudentsToIsland can't be null");
         }
-        if (victory) {
-            Player winner = turnManager.getPlayers().stream().min((player1, player2) -> {
-                if (player1.getBoard().getTowers().getCurrentSize() == player2.getBoard().getTowers().getCurrentSize()) {
-                    int prof1 = 0;
-                    for (int i = 0; i < FactionColor.values().length; i++) {
-                        if (player1.getBoard().getProfTable()[i]) prof1++;
-                    }
-                    int prof2 = 0;
-                    for (int i = 0; i < FactionColor.values().length; i++) {
-                        if (player2.getBoard().getProfTable()[i]) prof1++;
-                    }
-                    return Integer.compare(prof1, prof2);
-                } else {
-                    return Integer.compare(player1.getBoard().getTowers().getCurrentSize(), player2.getBoard().getTowers().getCurrentSize());
-                }
-            }).orElse(null);
-            throw new WinningException(winner);
+        final int maxForMovement = 3;
+        if (container.size() > maxForMovement) throw new RuntimeException();
+        Island island = islandsManager.getIslands().stream().
+                filter(island1 -> island1.getIslandId() == islandId)
+                .findFirst()
+                .orElseThrow();
+        turnManager.getCurrentPlayer().getBoard().getEntrance().removeContainer(container);
+        island.addStudents(container);
+    }
+
+    /**
+     * Move the students to the dining room of the current player
+     *
+     * @param container The students to move inside the dining room
+     * @throws IllegalArgumentException When the container is null
+     */
+    public void moveStudentsToDining(StudentsContainer container) throws IllegalArgumentException {
+        if (container == null) {
+            throw new IllegalArgumentException("container of moveStudentsToDining can't be null");
+        }
+        final int maxForMovement = 3;
+        if (container.size() > maxForMovement) throw new RuntimeException();
+        turnManager.getCurrentPlayer().getBoard().getEntrance().removeContainer(container);
+        turnManager.addStudentsToDining(container);
+    }
+
+    /**
+     * Register the used assistant inside the turn manager
+     *
+     * @param assistant The assistant played
+     * @throws AssistantImpossibleToPlay When the assistant is impossible to play because it's not in the desk of the
+     *                                   current player
+     * @throws IllegalArgumentException  When the assistant is null
+     */
+    public void playAssistant(Assistant assistant) throws AssistantImpossibleToPlay, IllegalArgumentException {
+        if (assistant == null) {
+            throw new IllegalArgumentException("Assistant must not be null");
+        }
+        try {
+            turnManager.nextTurnAssistant(assistant);
+        } catch (AssistantImpossibleToPlay exception) {
+            throw new AssistantImpossibleToPlay(exception);
         }
     }
 
     /**
+     * Move mother nature island by {@param stepForward} islands and activate the associated logic: checking conqueror
+     * of the island and if it is possible to unite islands
      *
+     * @param stepsForward The num of forward island movement of mother nature
      */
-    public void extractCharacter() {
+    public void moveMotherNature(int stepsForward) {
+        islandsManager.motherNatureActionMovement(stepsForward, turnManager.getPlayers());
+    }
 
+    /**
+     * Use a character and activate its effect; if it's the first usage increments its costs
+     *
+     * @param character Character played
+     */
+    public void playCharacter(Character character, Option option) {
+        Character used = Arrays.stream(characters).filter(character::equals).findFirst().orElseThrow();
+        used.useEffect(option);
     }
 }
