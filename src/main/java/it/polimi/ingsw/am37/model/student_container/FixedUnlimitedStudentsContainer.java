53fe007e1f0d75332ccb3366d1ba39ea796546b9
bramba2000 <54905332+bramba2000@users.noreply.github.com>
2022-05-12 16:30:32 +0200

    Add StudentsContainer#copy and StudentsContainer#getStudentsAsString  methods

--- src/main/java/it/polimi/ingsw/am37/model/student_container/FixedUnlimitedStudentsContainer.java
+++ src/main/java/it/polimi/ingsw/am37/model/student_container/FixedUnlimitedStudentsContainer.java
@@ -2,6 +2,8 @@ package it.polimi.ingsw.am37.model.student_container;
 
 import it.polimi.ingsw.am37.model.FactionColor;
 
+import java.util.Arrays;
+
 /**
  * Container for students tile, only adding students is allowed
  */
@@ -26,5 +28,17 @@ public class FixedUnlimitedStudentsContainer extends StudentsContainer {
         if (num < 0) throw new IllegalArgumentException("Num must be an int >= 0 but is " + num);
         if (color == null) throw new IllegalArgumentException("color is null");
         student[color.getIndex()] += num;
+    }
+
+    /**
+     * Return a copy of this container
+     *
+     * @return a student container that is the exact copy of this
+     */
+    @Override
+    public StudentsContainer copy() {
+        FixedUnlimitedStudentsContainer container = new FixedUnlimitedStudentsContainer();
+        container.student = Arrays.copyOf(this.student, this.student.length);
+        return container;
     }
 }
