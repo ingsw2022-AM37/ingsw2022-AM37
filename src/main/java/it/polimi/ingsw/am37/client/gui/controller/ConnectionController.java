ce2ac8bcee827a3f393e7ab5188187e911d76c44
Matteo-Baggetto <100494135+Matteo-Baggetto@users.noreply.github.com>
2022-06-08 18:45:44 +0200

    GUI in development

--- src/main/java/it/polimi/ingsw/am37/client/gui/controller/ConnectionController.java
+++ src/main/java/it/polimi/ingsw/am37/client/gui/controller/ConnectionController.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw.am37.client.gui.controller;
 import it.polimi.ingsw.am37.client.Client;
 import it.polimi.ingsw.am37.client.gui.SceneController;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -14,8 +16,18 @@ public class ConnectionController extends GenericController {
     private Client.ConnectionParameters connectionParameters;
 
     public void connect(ActionEvent actionEvent) {
-        connectionParameters = new Client.ConnectionParameters(ipField.getText(),
-                Integer.parseInt(portField.getText()));
+        if (ipField.getText().trim().replaceAll(" ", "").equals("") || portField.getText().trim().replaceAll(" ", "").equals("")) {
+            connectionParameters = new Client.ConnectionParameters("localhost", Integer.parseInt("60000"));
+        } else {
+            try {
+                Integer.parseInt(portField.getText());
+            } catch (NumberFormatException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid port input", ButtonType.OK);
+                alert.show();
+                return;
+            }
+            connectionParameters = new Client.ConnectionParameters(ipField.getText(), Integer.parseInt(portField.getText()));
+        }
         synchronized (SceneController.waitObject) {
             SceneController.waitObject.notifyAll();
         }
--- src/main/java/it/polimi/ingsw/am37/client/gui/controller/EnterInGameController.java
+++ src/main/java/it/polimi/ingsw/am37/client/gui/controller/EnterInGameController.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.am37.client.gui.controller;
 
 import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXToggleButton;
 import it.polimi.ingsw.am37.client.Client;
 import it.polimi.ingsw.am37.client.gui.SceneController;
 import javafx.event.ActionEvent;
@@ -9,7 +10,7 @@ import javafx.scene.control.TextField;
 
 public class EnterInGameController extends GenericController {
     public TextField nicknameField;
-    public TextField numOfPlayersField;
+    public JFXToggleButton numOfPlayersField;
     public JFXCheckBox advancedRulesField;
     public Label errorLabel;
 
@@ -17,9 +18,9 @@ public class EnterInGameController extends GenericController {
     private Client.LobbyParameters lobbyParameters;
 
     public void enterInGame(ActionEvent actionEvent) {
-        nickname = nicknameField.getText();
+        nickname = nicknameField.getText().trim().replaceAll(" ", "");
         lobbyParameters = new Client.LobbyParameters(advancedRulesField.isSelected(),
-                Integer.parseInt(numOfPlayersField.getText()));
+                numOfPlayersField.isSelected() ? 3 : 2);
         synchronized (SceneController.waitObject) {
             SceneController.waitObject.notifyAll();
         }
