ea09196aa3141bccf0312eda98a40dc75949e75f
bramba2000 <54905332+bramba2000@users.noreply.github.com>
2022-06-28 11:57:17 +0200

    Fix bug in closing color dialog - when one color is clicked now the window close

--- src/main/java/it/polimi/ingsw/am37/client/gui/controller/ChooseColorController.java
+++ src/main/java/it/polimi/ingsw/am37/client/gui/controller/ChooseColorController.java
@@ -3,12 +3,14 @@ package it.polimi.ingsw.am37.client.gui.controller;
 import it.polimi.ingsw.am37.model.FactionColor;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class ChooseColorController {
-    private FactionColor color;
+    private FactionColor color = null;
 
     public void colorClicked(MouseEvent mouseEvent) {
         color = FactionColor.valueOf(((Node) mouseEvent.getSource()).getId());
+        ((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow()).close();
     }
 
     public FactionColor getColor() {
